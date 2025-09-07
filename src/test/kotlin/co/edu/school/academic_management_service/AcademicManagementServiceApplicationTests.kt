0dc068345b81508c19f55cace4836bb327b6b2f7
Jorge A. Romero <jorgeandresromerog@gmail.com>
2025-09-07 18:21:32 -0500

    Initial commit: Spring Boot Kotlin academic management service

---
+++ src/test/kotlin/co/edu/school/academic_management_service/AcademicManagementServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.edu.school.academic_management_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AcademicManagementServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
